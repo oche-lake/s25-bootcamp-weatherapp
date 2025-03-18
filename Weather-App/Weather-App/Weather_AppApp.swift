ae0910c65744220171eaa46e20da49b659a6ed01
Ocean Chen <oche@unc.edu>
2025-03-18 11:08:13 -0400

    Created repo.

---
+++ Weather-App/Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Ocean Chen on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
